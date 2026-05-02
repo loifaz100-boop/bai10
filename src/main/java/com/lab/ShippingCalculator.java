61753479ac5fb98c37e3f6664a7d3b36e373062f
Tang Xuan Loi <loifaz100@gmail.com>
2026-05-02 17:19:52 +0700

    loi4

--- src/main/java/com/lab/ShippingCalculator.java
+++ src/main/java/com/lab/ShippingCalculator.java
@@ -7,7 +7,8 @@ public class ShippingCalculator {
             throw new IllegalArgumentException("Weight must be positive");
         }
         if (type.equals("EXPRESS")) return weight * 5000 + 20000;
-        if (type.equals("STANDARD")) return weight * 3000;
+        // Đổi dòng này
+        if (type.equals("STANDARD")) return weight * 4000; // Cố tình sửa 3000 thành 4000
         throw new IllegalArgumentException("Unknown type: " + type);
     }
 }
